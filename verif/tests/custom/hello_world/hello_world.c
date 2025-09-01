1f7601f0723034c068d0da024b39f624ec1028a3
Valedd99 <didomenico.valerio@virgilio.it>
2025-09-01 17:29:23 +0200

    Added tests for MPT walking

--- verif/tests/custom/hello_world/hello_world.c
+++ verif/tests/custom/hello_world/hello_world.c
@@ -15,15 +15,93 @@
 ** limitations under the License.
 **
 */
-#include <stdint.h>
+
+/*#include <stdint.h>
 #include <stdio.h>
 
+int main(int argc, char* arg[]) {
+	
+	printf("%d: Hello World !", 0);
+	
+	int a = 0;
+	for (int i = 0; i < 5; i++)
+	{
+		a += i;
+	}
+	return 0;
+}*/
+
+#include <stdint.h>
+
+/* ---- Dichiarazione funzione S-mode ---- */
+/* Mettiamo la funzione in una sezione dedicata .s_text */
+void s_function() __attribute__((section(".s_text")));
+void s_function() {
+    volatile int a = 42;  // operazioni dummy
+    a++;                  // esempio di uso della memoria
+    while(1);             // loop infinito per test
+}
+
+/* ---- Ottenere automaticamente l'indirizzo della funzione S-mode ---- */
+extern void s_function();
+#define S_FUNC_ADDR ((uintptr_t)&s_function)
+
+/* ---- Salta alla funzione S-mode configurando MPP e mepc ---- */
+void jump_to_s() {
+    uintptr_t ms;
+    __asm__ volatile ("csrr %0, mstatus" : "=r"(ms));
+    ms &= ~(3UL << 11);       // pulisce MPP
+    ms |=  (1UL << 11);       // MPP = S-mode (1)
+    __asm__ volatile ("csrw mstatus, %0" :: "r"(ms));
+
+    __asm__ volatile ("csrw mepc, %0" :: "r"(S_FUNC_ADDR));
+    //__asm__ volatile ("csrw stvec, %0" :: "r"(S_FUNC_ADDR));
+    __asm__ volatile ("mret" ::: "memory");
+}
+
+/* ---- Main ---- */
 int main() {
-    uint64_t write_val = 0x3000000000080025;
-    uint64_t read_val;
-    uint64_t addr = 0x80000000;
-    uint64_t sc_result;
- 
+    uint64_t write_val = 0x3000000000081000;
+    // PMP configuration
+    asm volatile (
+        "li t0, 0x80000000 >> 0\n"   // indirizzo base >>2
+        "csrw pmpaddr0, t0\n"
+        "li t0, 0x90000000 >> 0\n"
+        "csrw pmpaddr1, t0\n"
+        "li t0, (0b01 << 3) | 0b111\n" // TOR mode + R/W/X
+        "csrw pmpcfg0, t0\n"
+    );
+    uint64_t *mpt_entry1 = (uint64_t *)0x81000000;
+/*
+    //******************* 1-walking level *****************************
+    
+    // Leaf-entry --> access allowed
+    //*mpt_entry1 = 0x03FFFFFFFFFFFC03ULL;
+*/
+
+/*
+    //******************* 2-walking levels *****************************
+
+    // Non-leaf entry
+    *mpt_entry1 = 0x0000000024000001ULL;
+    // Leaf-entry --> access allowed  
+    uint64_t *mpt_entry2 = (uint64_t *)0x90000000;
+    *mpt_entry2 = 0x3FFFFFFFFFFFC03ULL;
+*/
+
+/*
+    //******************* 3-walking levels *****************************
+
+    // Non-leaf entry
+    *mpt_entry1 = 0x0000000024000001ULL;
+    // Non-leaf-entry
+    uint64_t *mpt_entry2 = (uint64_t *)0x90000000;
+    *mpt_entry2 = 0x0000000024000001ULL;
+    // Leaf-entry --> access allowed
+    uint64_t *mpt_entry3 = (uint64_t *)0x90000200;
+    *mpt_entry3 = 0x3FFFFFFFFFFFC03ULL;
+*/
+    // Enable MPT via MMPT register
     __asm__ volatile (
         "csrw 0x7C3, %0"
         :
@@ -31,114 +109,92 @@ int main() {
         : "memory"
     );
 
-    __asm__ volatile (
-    // Load-Reserved (Exclusive Read)
-    "lr.d %[read], (%[addr])\n\t"
-    // Try Exclusive Store
-    "sc.d %[result], %[val], (%[addr])\n\t"
-    : [read] "=&r"(read_val), [result] "=&r"(sc_result)
-    : [addr] "r"(addr), [val] "r"(write_val)
-    : "memory"
-    );
-
-/*
-    
-    // Genera un'eccezione di tipo ST_ADDR_MISALIGNED (cause = 6)
-    volatile uint64_t *misaligned_addr = (uint64_t *)(0x80001003); // NON allineato a 8 byte
-
-    __asm__ volatile (
-        "sd %0, 0(%1)\n\t"     // Store word su indirizzo non allineato
-        :
-        : "r"(write_val), "r"(misaligned_addr)
-        : "memory"
-    );
-  */  
-    /*
-    __asm__ volatile (
-        "csrw 0x7C3, %0\n\t"
-        ".word 0xFFFFFFFF\n\t"   // Istruzione illegale
-        :
-        : "r"(write_val)
-        : "memory"
-    );
-    */
-
-    /*
-    volatile uint64_t *addr1 = (uint64_t *)0x80001000;
-    volatile uint64_t *addr2 = (uint64_t *)0x80001008;
-
-    uint64_t val1, val2;
-
-    // Eseguiamo due operazioni di load
-    __asm__ volatile (
-        "ld %0, 0(%1)\n\t"
-        "ld %2, 0(%3)\n\t"
-        : "=r"(val1), "=r"(val2)
-        : "r"(addr1), "r"(addr2)
-        : "memory"
-    );
-*/
+    jump_to_s();
     return 0;
 }
 
-/*
-#include <stdint.h>
-#include <stdio.h>
+// 
+// #include <stdint.h>
+// 
+// int main(int argc, char* arg[]) {
+//     uint64_t write_val = 0x3000000000080025;
+//     volatile int a = 1234;
+//     int loaded_val;
+//     int store_result;
+//     int new_val = 5678;
+// 
+//     // Pointer to 'a'
+//     int* a_ptr = (int*)&a;
+// 
+//     __asm__ volatile (
+//         "csrw 0x7C3, %0"
+//         :
+//         : "r"(write_val)
+//         : "memory"
+//     );
+// 
+//     // Use inline asm with lr.w and sc.w
+//     __asm__ volatile (
+//         "lr.w %[val], (%[addr])\n"         // Load-reserved into val
+//         "sc.w %[res], %[newval], (%[addr])\n" // Try store-conditional newval into a
+//         : [val] "=&r"(loaded_val),         // output: value loaded from memory
+//           [res] "=&r"(store_result)        // output: result of store-conditional (0 = success)
+//         : [addr] "r"(a_ptr),               // input: address of 'a'
+//           [newval] "r"(new_val)            // input: value to store
+//         : "memory"
+//     );
+//     
+//     /* ---- Switch a HS/S-mode e ritorno con mret ---- */
+//     uintptr_t ms;
+//         /* Leggi mstatus */
+//         __asm__ volatile ("csrr %0, mstatus" : "=r"(ms));
+//         /* MPP è in mstatus[12:11]: pulisci e metti 00 (U-mode) */
+//         ms &= ~(3UL << 11);     // clear MPP
+//         ms |=  (1UL << 11);     // set MPP=00 => U-mode
+//         /* Scrivi mstatus aggiornato */
+//         __asm__ volatile ("csrw mstatus, %0" :: "r"(ms));
+// //
+// //        /* Imposta mepc all'etichetta 1f e fai mret */
+// //        __asm__ volatile (
+// //            "la t0, 1f\n"       /* carica indirizzo dell'etichetta 1 */
+// //            "csrw mepc, t0\n"   /* mepc <- 1f */
+// //            "mret\n"            /* passa a HS/S-mode e salta a 1f   */
+// //            "1:\n"
+// //            :
+// //            :
+// //            : "t0", "memory"
+// //        );
+// 
+//         __asm__ volatile (
+//             "csrw mepc, %0\n"
+//             :
+//             : "r"(0x000000008000306CUL)
+//             : "memory"
+//         );
+// 
+// /*
+//         __asm__ volatile (
+//             "csrw mepc, %0\n"
+//             :
+//             : "r"(0x0000000080003000UL)
+//             : "memory"
+//         );
+// */
+// 
+//         __asm__ volatile (
+//             "mret\n"
+//             :
+//             :
+//             : "memory"
+//         );
+// 
+// 
+//     return 0;
+// 
+// }
 
-int main(int argc, char* arg[]) {
-	
-    __asm__ volatile (
-        "li t0, 1\n"            
-        "csrw 0x7C3, t0\n"      
-    );
-	// printf("%d: Hello World !", 0);
-	
-	
-    uint64_t value;
 
-    __asm__ volatile (
-        "csrr %0, 0x7C3"
-        : "=r"(value)
-        :
-        : "memory"
-    );
-
-     if (value == 1){
-        int a = 0;
-        for (int i = 0; i < 5; i++)
-        {
-            a += i;
-        }
-     }
-
-	return 0;
-}
-*/
-/*
-#include <stdint.h>
-
-int main(int argc, char* arg[]) {
-    volatile int a = 1234;
-    int loaded_val;
-
-    // Pointer to 'a'
-    int* a_ptr = (int*)&a;
-
-    // Use inline asm with lr.w
-    __asm__ volatile (
-        "lr.w %0, (%1)\n"
-        : "=r"(loaded_val)      // output: value loaded from memory
-        : "r"(a_ptr)            // input: address of a in a register
-        : "memory"
-    );
-
-    for (int i = 0; i < 5; i++) {
-        a += i;
-    }
-
-    return 0;
-}
-*/
+// ***********************************  AMO INSTRUCTIONS **********************************************
 /*
 #include <stdint.h>
 
@@ -188,50 +244,6 @@ int main(int argc, char* argv[]) {
     tmp = 0xFFFF;
     __asm__ volatile (
         "amoxor.w %0, %2, (%1)\n"
-        : "=r"(loaded_val)
-        : "r"(a_ptr), "r"(tmp)
-        : "memory"
-    );
-
-    return 0;
-}
-*/
-
-/*
-#include <stdint.h>
-
-int main(int argc, char* argv[]) {
-    volatile int a = 1234;
-    int loaded_val, store_success, tmp;
-
-    // Pointer to 'a'
-    int* a_ptr = (int*)&a;
-
-    // --- Store normale (SW) ---
-    tmp = 5678;
-    *a_ptr = tmp;  // Questo sarà il riferimento "normale" per confronto con SC
-
-    // --- Store Condizionato (SC.W) ---
-    // Finta riserva di memoria con LR.W
-    __asm__ volatile (
-        "lr.w %0, (%1)\n"
-        : "=r"(loaded_val)
-        : "r"(a_ptr)
-        : "memory"
-    );
-
-    tmp = 4321; // valore che vogliamo scrivere condizionatamente
-    __asm__ volatile (
-        "sc.w %0, %2, (%1)\n"
-        : "=r"(store_success)
-        : "r"(a_ptr), "r"(tmp)
-        : "memory"
-    );
-
-    // --- Atomic Swap (AMOSWAP.W) ---
-    tmp = 999;
-    __asm__ volatile (
-        "amoswap.w %0, %2, (%1)\n"
         : "=r"(loaded_val)
         : "r"(a_ptr), "r"(tmp)
         : "memory"
