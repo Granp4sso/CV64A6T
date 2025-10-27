ba501eec499c75e5835ae2757463313a66503f6f
Valedd99 <didomenico.valerio@virgilio.it>
2025-10-27 11:52:22 +0100

    Quick fix to mptw_test.c

--- verif/tests/custom/mptw_test/mptw_test.c
+++ verif/tests/custom/mptw_test/mptw_test.c
@@ -4,25 +4,6 @@
    This code is used to test MPTWs of PTW, Load Unit, Store Unit and IF Unit.
 */
 
-
-//*************************************** DEFAULT *************************************** 
-/*
-#include <stdint.h>
-#include <stdio.h>
-
-int main(int argc, char* arg[]) {
-	
-	printf("%d: Hello World !", 0);
-	
-	int a = 0;
-	for (int i = 0; i < 5; i++)
-	{
-		a += i;
-	}
-	return 0;
-}
-*/
-
 //*************************************** MPT TESTS *************************************** */
 
 #include <stdint.h>
